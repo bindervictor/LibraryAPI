5a985b6a27b30aaeb3f0ebc2c01faa96c66d6028
victor-binder <victorbinder99@gmail.com>
2025-02-13 18:36:49 -0300

    update gitignore

---
+++ library-app/.angular/cache/19.1.6/library-app/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-63L36653.js";
+import "./chunk-KA3SISMW.js";
+import "./chunk-ZJCMGGPL.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
---
+++ library-app/.angular/cache/19.1.6/library-app/vite/deps/@angular_cdk_text-field.js
@@ -0,0 +1,16 @@
+import {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+} from "./chunk-VJRUGRFH.js";
+import "./chunk-ZX5O2Z46.js";
+import "./chunk-2K734OQY.js";
+import "./chunk-KA3SISMW.js";
+import "./chunk-ZJCMGGPL.js";
+export {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+};
